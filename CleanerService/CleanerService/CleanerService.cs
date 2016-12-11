c8c384922c92280cbc28737adc009ff188b0ab85
sriharsha sammeta <sriharsha4444@icloud.com>
2016-12-11 20:41:33 +0000

    Adding CLearning up service

---
+++ CleanerService/CleanerService/CleanerService.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanerService
+{
+    public partial class CleanerService : ServiceBase
+    {
+        System.Timers.Timer timer;
+        string baseDirectory, connectionString;
+        int startupInterval, lifeSpan;
+        public CleanerService()
+        {
+            InitializeComponent();
+            startupInterval = int.Parse(ConfigurationManager.AppSettings["StartupInterval"]);
+            lifeSpan = int.Parse(ConfigurationManager.AppSettings["LifeSpan"]);
+            baseDirectory = ConfigurationManager.AppSettings["BaseDirectory"];
+            connectionString = ConfigurationManager.ConnectionStrings["DatabaseConnString"].ConnectionString;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Deletefile();
+        }
+
+        protected void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Deletefile();
+        }
+        public void Deletefile()
+        {
+            try
+            {
+                timer = new System.Timers.Timer();
+                timer.Interval = (10000) * startupInterval;
+                timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+                List<string> fileNames = new List<string>();
+                foreach (string file in Directory.GetFiles(baseDirectory, "*", SearchOption.AllDirectories))
+                {
+                    if (DateTime.Now.Subtract(File.GetCreationTime(file)).TotalDays > lifeSpan)
+                    {
+                        File.Delete(file);
+                        fileNames.Add(file);
+                    }
+                }
+                if (fileNames.Count > 0)
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        string combinedFileNames = string.Join(",", fileNames);
+                        string deleteQuery = "Delete From PrintJob Where FileName IN (@files)";
+                        SqlCommand deleteCommand = new SqlCommand(deleteQuery, connection);
+                        deleteCommand.Parameters.AddWithValue("@files", combinedFileNames);
+                        deleteCommand.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                }
+                timer.Enabled = true;
+                timer.Start();
+            }
+            catch (Exception e)
+            {
+                string sSource;
+                string sLog;
+                sSource = "Cleaner Service";
+                sLog = "Application";
+                if (!EventLog.SourceExists(sSource))
+                    EventLog.CreateEventSource(sSource, sLog);
+                string errorMessage = e.Message + "\n\n";
+                while (e.InnerException != null)
+                {
+                    errorMessage += e.InnerException + "\n";
+                    e = e.InnerException;
+                }
+                EventLog.WriteEntry(sSource, errorMessage, EventLogEntryType.Error);                
+            }
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ CleanerService/CleanerService/ProjectInstaller.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace CleanerService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CleanerServiceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.CleanerServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // CleanerServiceProcessInstaller
+            // 
+            this.CleanerServiceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.CleanerServiceProcessInstaller.Password = null;
+            this.CleanerServiceProcessInstaller.Username = null;
+            // 
+            // CleanerServiceInstaller
+            // 
+            this.CleanerServiceInstaller.ServiceName = "CleanerService";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.CleanerServiceProcessInstaller,
+            this.CleanerServiceInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller CleanerServiceProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller CleanerServiceInstaller;
+    }
+}
